3ccbd2b801060aed685abce20aa66a05b67987f0
KaterinaSinicka-221RDB179 <125562888+KaterinaSinicka-221RDB179@users.noreply.github.com>
2023-03-19 20:58:25 +0000

    Katerina Sinicka 221RDB179

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # python3
+import heapq
 
 def parallel_processing(n, m, data):
-    output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-    next_free_time = [0] * n
-    output = []
-    for job_id in range(m):
-        min_thread_id = 0
-        for i in range(1, n):
-            if next_free_time[i] < next_free_time[min_thread_id]:
-                min_thread_id = 1
-    result.append((min_thread_id, next_free_time[min_thread_id]))
-    next_free_time[min_thread_id] += data[job_id]
+    heap = [(0,i) for i in range(n)]
+    output = [(0,0) for _ in range(m)]
+
+    for i in range(n,m):
+        time, thread = heapq.heappop(heap)
+        output[i] = (thread, time)
+        heapq.heappush(heap, (time + data[i], thread))
+    while heap:
+        time, thread = heapq.heappop(heap)
+        output[m-n+thread] = (thread, time)
 
     return output
 
@@ -22,8 +23,8 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    #n = 0
+    #m = 0
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
 
@@ -31,11 +32,11 @@ def main():
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
+    output = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
-    for thread_id, start_time in output:
-        print(thread_id, start_time)
+    for thread, time in output:
+        print(thread, time)
 
 
 
